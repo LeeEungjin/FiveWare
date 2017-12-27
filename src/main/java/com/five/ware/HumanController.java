aaa3b11963b73b0e6a13ca6e973f42985f72fc88
a-rin2 <34049818+a-rin2@users.noreply.github.com>
2017-12-27 17:18:21 +0900

    sample

---
+++ src/main/java/com/five/ware/HumanController.java
@@ -0,0 +1,13 @@
+package com.five.ware;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HumanController {
+
+	@RequestMapping(value="/")
+	public String humanplus(){
+		return "human_memberplus";
+	}
+}
