591c8fdc8c9a546131ffb83cec182853644807f5
jihyun927 <enddl1026@hanmail.net>
2018-01-02 12:52:07 +0900

    commit

--- src/main/java/com/five/ware/erp/menuRegist/MenuRegistDAO.java
+++ src/main/java/com/five/ware/erp/menuRegist/MenuRegistDAO.java
@@ -1,11 +1,18 @@
 package com.five.ware.erp.menuRegist;
 
+import java.util.List;
+
+import javax.inject.Inject;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Repository;
+
+import com.five.ware.util.RowNum;
+
 
 @Repository
 public class MenuRegistDAO {
 	
+	@Inject
 	private SqlSession sqlsession;
 	private static final String namespace="menuRegistMapper.";
 	
@@ -13,4 +20,11 @@ public class MenuRegistDAO {
 		return sqlsession.insert(namespace+"menuReigstInsert", menuRegistDTO);
 	}
 	
+	public List<MenuRegistDTO> selectList(RowNum rowNum)throws Exception{
+		return sqlsession.selectList(namespace+"selectList",rowNum);
+	}
+	
+	public int totalCount(RowNum rowNum )throws Exception{
+		return sqlsession.selectOne(namespace+"totalCount", rowNum);
+	}
 }
