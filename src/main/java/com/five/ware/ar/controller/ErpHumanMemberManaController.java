63ee4a5ad41bdcaf0cf0c98cc82590c151a98775
a-rin2 <34049818+a-rin2@users.noreply.github.com>
2018-01-03 21:14:47 +0900

    dd

---
+++ src/main/java/com/five/ware/ar/controller/ErpHumanMemberManaController.java
@@ -0,0 +1,15 @@
+package com.five.ware.ar.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping(value="/human/memberMana/**")
+public class ErpHumanMemberManaController {
+
+	@RequestMapping(value="memberPlus")
+	public void memberPlus() throws Exception{
+		
+	}
+}
