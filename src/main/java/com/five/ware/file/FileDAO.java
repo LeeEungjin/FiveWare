25451035e51a3128a7b170eefbeea9d17544707b
LeeEungjin <dmdwls0208@gmail.com>
2018-01-09 17:28:28 +0900

    arin_file

--- src/main/java/com/five/ware/file/FileDAO.java
+++ src/main/java/com/five/ware/file/FileDAO.java
@@ -15,6 +15,17 @@ public class FileDAO {
 	private SqlSession sqlSession;
 	private final String NAMESPACE="fileMapper.";
 	
+	public int fileInsert(FileDTO fileDTO) throws Exception{
+      int result = sqlSession.insert(NAMESPACE+"fileInsert", fileDTO);
+      
+      return result;
+    }
+	   
+   public FileDTO fileOne(String code) throws Exception{
+      FileDTO fileDTO = sqlSession.selectOne(NAMESPACE+"fileOne",code);
+      
+      return fileDTO;
+   }
 	
 	public int getFnum() throws Exception {
 		return sqlSession.selectOne(NAMESPACE+"getFnum");
