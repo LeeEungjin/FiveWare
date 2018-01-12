0db20f089fdb800890e4103e8520ccc1733d9fbd
a-rin2 <34049818+a-rin2@users.noreply.github.com>
2018-01-12 16:49:26 +0900

    22

--- src/main/java/com/five/ware/ExcelController.java
+++ src/main/java/com/five/ware/ExcelController.java
@@ -2,12 +2,17 @@ package com.five.ware;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.inject.Inject;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.five.ware.erp.human.dili.MemberWorkDAO;
+import com.five.ware.erp.human.dili.MemberWorkDTO;
+import com.five.ware.erp.human.dili.MemberWorkService;
 import com.five.ware.erp.product.ProductDTO;
 import com.five.ware.excel.ExcelRoom;
 import com.five.ware.excel.ExcelView2003;
@@ -15,6 +20,11 @@ import com.five.ware.excel.ExcelView2003;
 @Controller
 @RequestMapping(value="/excel/**")
 public class ExcelController {
+	
+	@Inject
+	private MemberWorkDAO memberWorkDAO;
+	@Inject
+	private MemberWorkService memberWorkService;
 	
 	@RequestMapping(value="exportExcel2003", method=RequestMethod.GET)
 	public ModelAndView downloadExcel2003(HttpServletResponse response){
@@ -54,4 +64,41 @@ public class ExcelController {
 		
 		return new ModelAndView("excelView2007", "listRooms", listRooms);
 	}
+	
+	@RequestMapping(value="diliSearchExcel", method=RequestMethod.GET)
+	public ModelAndView diliSearchExcel(HttpServletResponse response, String search) throws Exception{
+		
+		response.setHeader("Content-disposition", "attachment; filename=" + "diliSearchExcel" + ".xls");
+	/*	
+		List<MemberWorkDTO> ar = memberWorkDAO.mWorkList(search);// 사원
+		List<String> ar2 = memberWorkDAO.diliNameList(); // 근태목록
+		List<Integer> sumList = null;
+		List<List<Integer>> result = new ArrayList<List<Integer>>();
+		
+		for(MemberWorkDTO member : ar){
+			sumList=new ArrayList<Integer>();
+			for(String dili : ar2){
+				int sum = memberWorkDAO.mSumList(member.getCode(), dili);
+				
+				sumList.add(sum);
+			}
+			
+			result.add(sumList);
+		}
+		*/
+		
+		ModelAndView mv = new ModelAndView();
+		
+		memberWorkService.mWorkList(search, mv);
+		
+		mv.setViewName("excelDiliSearch2007");
+		
+		
+		return mv;
+	}
+	
+	
+	
+	
+	
 }
